c65a67f7a29ce6c0286c72581d607ff5f52bc9fe
YAMADA-PC\Yuhara <yamada@insel-art.jp>
2020-10-30 17:56:05 +0900

    add shortcode.php

--- functions.php
+++ functions.php
@@ -3,6 +3,7 @@
  * 設定ファイルの読み込み
  */
 require_once(dirname(__FILE__) . '/config/page-template-hierarchy.php');
+require_once(dirname(__FILE__) . '/config/shortcode.php');
 require_once(dirname(__FILE__) . '/config/image-size.php');
 require_once(dirname(__FILE__) . '/config/source.php');
 require_once(dirname(__FILE__) . '/config/components.php');
